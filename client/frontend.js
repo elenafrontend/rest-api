cb2964a98a8ecfc686b74c1789c29771abd091cc
elena.g <gumerova.e.p@ya.ru>
2021-07-10 18:28:52 +0300

    init commit

---
+++ client/frontend.js
@@ -0,0 +1,24 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js";
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      form: {
+        name: "",
+        value: "",
+      },
+      contacts: [],
+    };
+  },
+  methods: {
+    createContact() {
+      const { ...contact } = this.form;
+
+      this.contacts.push({ ...contact, id: Date.now() });
+      console.log(this.contacts);
+
+      this.form.name = this.form.value = "";
+    },
+  },
+});
